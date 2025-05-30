5cd03ea612a20fd296b7cf8a35dcfc6bfacd8f3c
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-05-30 09:56:34 -0500

    :sparkles: Added hello service

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.HelloWorldService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
 
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World";
+        return helloWorldService.getHelloMessage();
     }
 }
